099f76d7880799eb96d6f9efb829f6b6785df75f
YJ <coffee0101@kakao.com>
2022-05-23 12:25:55 +0900

    first commit

---
+++ server.py
@@ -0,0 +1,25 @@
+import socket
+import threading
+import sqlite3
+
+PORT = 2090
+BUF_SIZE = 1024
+lock = threading.Lock()
+clnt_imfor = []
+clnt_cnt = 0
+
+con = sqlite3.connect('Book.db')
+
+c = con.cursor()
+
+if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', PORT))
+    sock.listen(5)
+
+    while True:
+        clnt_sock, addr = sock.accept()
+
+        lock.acquire()
+
+        lock.release()
