6451bd4731157f96d445319edcf8d4a76da252cf
Bang <saverock1235@gmail.com>
2022-05-25 11:14:59 +0900

    update alert

--- Book_client.py
+++ Book_client.py
@@ -20,7 +20,6 @@ check_msg = ""
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((IP, Port))
 
-
 def send_email_to_clnt(self):  # 이메일 체크 시작
     global check_msg
     email = self.email_Edit.text()
@@ -30,16 +29,28 @@ def send_email_to_clnt(self):  # 이메일 체크 시작
         ses = smtplib.SMTP('smtp.gmail.com', 587)  # smtp 세션 설정
         ses.starttls()
         # 이메일을 보낼 gmail 계정에 접속
-        ses.login('saverock1235@gmail.com', "ohjo tynp pojs xjcx")
+        ses.login('uihyeon.bookstore@gmail.com', 'ttqe mztd lljo tguh')
 
         check_msg = str(random.randrange(1000, 10000))
         msg = MIMEText('인증번호: '+check_msg)  # 보낼 메세지 내용을 적는다
         msg['subject'] = '의혀닝책방에서 인증코드를 발송했습니다.'  # 보낼 이메일의 제목을 적는다
         # 앞에는 위에서 설정한 계정, 두번째에는 이메일을 보낼 계정을 입력
-        ses.sendmail("saverock1235@gmail.com", email, msg.as_string())
+        ses.sendmail('uihyeon.bookstore@gmail.com', email, msg.as_string())
+        result_value = "success"
+    else: 
+        QMessageBox.question(self.window, 'error', '이메일 형식이 아닙니다.\n다시 시도해주세요.', QMessageBox.Yes, QMessageBox.NoButton)
+        result_value = "fail"
+        
+    ses.quit() # 이메일 체크 종료
+    return result_value
 
-        ses.quit()
-# 이메일 체크 종료
+def check_rcv():
+    while True:
+        ck = sock.recv(BUF_SIZE)
+        ck = ck.decode()
+        if sys.getsizeof(ck) >= 1:
+            break
+    return ck
 
 
 class Login(QDialog):  # 로그인창 시작
@@ -90,15 +101,12 @@ class id_Find(QDialog):  # 아이디찾기 시작
         e_mail = self.email_Edit.text()
         e_mail = "find_id/"+e_mail
         sock.send(e_mail.encode())
-        while True:
-            ck = sock.recv(BUF_SIZE)
-            ck = ck.decode()
-            if sys.getsizeof(ck) >= 1:
-                break
+        ck = check_rcv()
         if ck == "!OK":  # 아이디 중복확인이 완료했을시 입력칸 잠금해제
-            send_email_to_clnt(self)
-            self.emailnum_Edit.setEnabled(True)
-            self.email_C_Btn_2.setEnabled(True)
+            func_result = send_email_to_clnt(self)
+            if func_result == "success":
+                self.emailnum_Edit.setEnabled(True)
+                self.email_C_Btn_2.setEnabled(True)
 
     def check_code(self):
         ck_code = self.emailnum_Edit.text()
@@ -116,29 +124,31 @@ class pw_Find(QDialog):  # 비밀번호찾기 시작
         super().__init__()
         self.ui = uic.loadUi("find_pw.ui", self)
 
-        self.id_Btn.clicked.connect(self.check_email)
+        self.id_Btn.clicked.connect(self.check_id)
         self.email_Btn_2.clicked.connect(self.send_email)
         self.email_C_Btn_2.clicked.connect(self.check_E_num)
         self.join_Btn.clicked.connect(self.end)
 
-    def check_email(self):
-        e_mail = self.id_Edit.text()  # 텍스트창에 있는걸 id 변수에 넣는다
-        e_mail = "find_pw/"+e_mail
+    def check_id(self):
+        id = self.id_Edit.text()  # 텍스트창에 있는걸 id 변수에 넣는다
+        id = "find_pw/"+id
         sock.send(id.encode())
-        while True:
-            ck = sock.recv(BUF_SIZE)
-            ck = ck.decode()
-            if sys.getsizeof(ck) >= 1:
-                break
+        ck = check_rcv()
         if ck == "!OK":  # 아이디 중복확인이 완료했을시 입력칸 잠금해제
             self.email_Edit.setEnabled(True)
             self.email_Btn.setEnabled(True)
+        else:
+            QMessageBox.question(self.window, 'error', '존재하지 않는 아이디입니다.\n다시 시도해주세요.', QMessageBox.Yes, QMessageBox.NoButton)
 
     def send_email(self):
-        send_email_to_clnt(self)
-
-        self.emailnum_Edit.setEnabled(True)
-        self.email_C_Btn.setEnabled(True)
+        email = self.email_Edit.text()
+        sock.send(email.encode())
+        ck = check_rcv()
+        if ck == "!OK":  # 아이디 중복확인이 완료했을시 입력칸 잠금해제
+            func_result = send_email_to_clnt(self)
+            if func_result == "success":
+                self.emailnum_Edit.setEnabled(True)
+                self.email_C_Btn_2.setEnabled(True)
 
     def check_E_num(self):
         check_num = self.emailnum_Edit.text()
@@ -166,16 +176,13 @@ class reg(QDialog):  # 가입창 시작
     def check_id(self):
         id = self.id_Edit.text()  # 텍스트창에 있는걸 id라는 변수에 집어넣는다
         sock.send(id.encode())
-
-        while True:
-            ck = sock.recv(BUF_SIZE)
-            ck = ck.decode()
-            if sys.getsizeof(ck) >= 1:
-                break
+        ck = check_rcv()
         if ck == "!OK":  # 아이디 중복확인이 완료했을시 입력칸 잠금해제
             self.pw_Edit.setEnabled(True)
             self.repw_Edit.setEnabled(True)
             self.pw_Btn.setEnabled(True)
+        else:
+            QMessageBox.question(self.window, 'error', '존재하지 않는 아이디입니다.\n다시 시도해주세요.', QMessageBox.Yes, QMessageBox.NoButton)
 
     def check_pw(self):
         a = self.pw_Edit.text()
@@ -184,12 +191,14 @@ class reg(QDialog):  # 가입창 시작
             self.name_Edit.setEnabled(True)
             self.email_Edit.setEnabled(True)
             self.email_Btn.setEnabled(True)
+        else: 
+            QMessageBox.question(self.window, 'error', '비밀번호가 일치하지 않습니다.\n다시 시도해주세요.', QMessageBox.Yes, QMessageBox.NoButton)
 
     def send_email(self):
-        send_email_to_clnt(self)
-
-        self.emailnum_Edit.setEnabled(True)
-        self.email_C_Btn.setEnabled(True)
+        func_result = send_email_to_clnt(self)
+        if func_result == "success":
+            self.emailnum_Edit.setEnabled(True)
+            self.email_C_Btn_2.setEnabled(True)
 
     def check_E_num(self):
         check_num = self.emailnum_Edit.text()
