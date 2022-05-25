1394082bef26c7b15368f8362a65de918622a920
Bang <saverock1235@gmail.com>
2022-05-25 10:04:45 +0900

    update find_pw

--- Book_client.py
+++ Book_client.py
@@ -19,6 +19,24 @@ check_msg=""
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((IP, Port))
 
+def send_email_to_clnt(self):
+    #이메일 체크
+    global check_msg
+    email = self.email_Edit.text()
+    check = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+    
+    if check.match(email) != None:
+        ses = smtplib.SMTP('smtp.gmail.com', 587) # smtp 세션 설정
+        ses.starttls()
+        ses.login('saverock1235@gmail.com', "ohjo tynp pojs xjcx") # 이메일을 보낼 gmail 계정에 접속
+        
+        check_msg = str(random.randrange(1000, 10000))
+        msg = MIMEText('인증번호: '+check_msg) # 보낼 메세지 내용을 적는다
+        msg['subject'] = '의혀닝책방에서 인증코드를 발송했습니다.' # 보낼 이메일의 제목을 적는다
+        ses.sendmail("saverock1235@gmail.com", email, msg.as_string()) # 앞에는 위에서 설정한 계정, 두번째에는 이메일을 보낼 계정을 입력
+
+        ses.quit() # 세선종료  
+
 class Login(QDialog): # 로그인 클래스
     def __init__(self):
         super().__init__()
@@ -84,22 +102,7 @@ class reg(QDialog): # 회원가입 클래스
             self.email_Edit.setEnabled(True)
             self.email_Btn.setEnabled(True)
     def send_email(self):
-        #이메일 체크
-        global check_msg
-        email = self.email_Edit.text()
-        check = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-        
-        if check.match(email) != None:
-            ses = smtplib.SMTP('smtp.gmail.com', 587) # smtp 세션 설정
-            ses.starttls()
-            ses.login('saverock1235@gmail.com', "ohjo tynp pojs xjcx") # 이메일을 보낼 gmail 계정에 접속
-            
-            check_msg = str(random.randrange(1000, 10000))
-            msg = MIMEText('인증번호: '+check_msg) # 보낼 메세지 내용을 적는다
-            msg['subject'] = '의혀닝책방에서 인증코드를 발송했습니다.' # 보낼 이메일의 제목을 적는다
-            ses.sendmail("saverock1235@gmail.com", email, msg.as_string()) # 앞에는 위에서 설정한 계정, 두번째에는 이메일을 보낼 계정을 입력
-
-            ses.quit() # 세선종료  
+        send_email_to_clnt(self)
                  
         self.emailnum_Edit.setEnabled(True)
         self.email_C_Btn.setEnabled(True)
@@ -121,8 +124,36 @@ class reg(QDialog): # 회원가입 클래스
 class find_pw(QDialog):
     def __init__(self):
         super().__init__()
-        self.ui = uic.loadUi("Login.ui",self)
+        self.ui = uic.loadUi("find_pw.ui",self)
 
+        self.id_Btn.clicked.connect(self.check_id)
+        self.email_Btn_2.clicked.connect(self.send_email)
+        self.email_C_Btn_2.clicked.connect(self.check_E_num)
+        
+    def check_id(self):
+        id=self.id_Edit.text()#텍스트창에 있는걸 id 변수에 넣는다
+        sock.send(id.encode())
+        while True:
+            ck = sock.recv(BUF_SIZE)
+            ck = ck.decode()
+            if sys.getsizeof(ck) >= 1:
+                break
+        if ck == "!OK" :
+            #아이디 중복확인이 완료했을시 입력칸 잠금해제
+            self.email_Edit.setEnabled(True)
+            self.email_Btn.setEnabled(True)
+            
+    def send_email(self):
+        send_email_to_clnt(self)
+                 
+        self.emailnum_Edit.setEnabled(True)
+        self.email_C_Btn.setEnabled(True)
+        
+    def check_E_num(self):    
+        check_num = self.emailnum_Edit.text()
+        if check_num == check_msg:
+            self.join_Btn.setEnabled(True)
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
